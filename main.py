5baaa8f972d2cc7c4c132a607f1bc7674f61cca9
Python Mega Course <tonyringertx@gmail.com>
2024-10-26 11:33:04 -0500

    get function working on localhost

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 from flask import Flask
 from flask_restful import Resource, Api
+
+
+app = Flask("VideoAPI")
+api = Api(app)
+
+class Video(Resource):
+
+    def get(self):
+        return "Hello World"
+
+
+api.add_resource(Video, '/')
+
+if __name__ == '__main__':
+    app.run()
